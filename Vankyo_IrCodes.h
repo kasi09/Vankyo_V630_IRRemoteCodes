16ea08be7c4b81daf422c2bba6353349dca84498
kasi09 <kasi09@gmail.com>
2021-08-21 09:49:09 +0200

    Add files via upload

---
+++ Vankyo_IrCodes.h
@@ -0,0 +1,21 @@
+
+#ifndef vankyo_ircodes_h_
+#define vankyo_ircodes_h
+
+#define     Power       0x57A8FF00
+#define     Mute        0x7788FF00
+#define     Prev        0x6798FF00
+#define     Play        0x6C93FF00
+#define     Next        0x7D82FF00
+#define     Up          0x6A95FF00
+#define     Left        0x649BFF00
+#define     Ok          0x619EFF00
+#define     Right       0x6699FF00
+#define     Down        0x659AFF00
+#define     Back        0x5BA4FF00
+#define     List        0x6E91FF00
+#define     Source      0x6897FF00
+#define     Vol_down    0x639CFF00
+#define     Vol_up      0x738CFF00
+
+#endif /* vankyo_ircodes_h */
